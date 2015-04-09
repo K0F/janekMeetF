a7f2dce8666b7ab6b4378f047b5c764faec2fabf
k0f <krystof.pesek@gmail.com>
2015-04-09 13:06:38 +0200

    working version

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /home/pi/.bashrc
+export PATH=$PATH:/usr/bin:/usr/local/bin:/sbin/
+
+(mkfifo /tmp/ctl)&
+
+((/usr/local/bin/nc -z -d -ul localhost -p7777) > /tmp/ctl)&
+
+(sleep 5s && omxplayer --loop --no-osd -b video.mp4 < /tmp/ctl)&
+
+#(sleep 30s && /home/pi/send.sh)&
+#(while true; do sleep 25m && sleep 13s && sh /home/pi/send.sh ;done)&
+
