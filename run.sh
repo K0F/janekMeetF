7cb4a19815edcc958130c1cfc227549a05063b73
k0f <krystof.pesek@gmail.com>
2015-04-09 15:53:22 +0200

    working loop

--- run.sh
+++ run.sh
@@ -6,7 +6,11 @@ export PATH=$PATH:/usr/bin:/usr/local/bin:/sbin/
 
 ((/usr/local/bin/nc -z -d -ul localhost -p7777) > /tmp/ctl)&
 
-(sleep 5s && omxplayer --loop --no-osd -b video.mp4 < /tmp/ctl)&
+while true; do
+    (sleep 4s && omxplayer -o local --no-osd /home/pi/video.mp4 < /tmp/ctl)
+done
+
+#(sleep 5s && omxplayer --loop --no-osd -b video.mp4 < /tmp/ctl)&
 
 #(sleep 30s && /home/pi/send.sh)&
 #(while true; do sleep 25m && sleep 13s && sh /home/pi/send.sh ;done)&
